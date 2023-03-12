62df6fb618950bc45bba1c0d015867e483625abd
SHREYANK <66244570+bshreyank@users.noreply.github.com>
2023-03-12 16:19:42 +0530

    newProb

---
+++ DiagnolDifference.java
@@ -0,0 +1,17 @@
+/**
+ * DiagnolDifference
+ */
+public class DiagnolDifference {
+
+    public static void main(String[] args) {
+        int[] num = {10,20,30,40,50,45,12,13,15};
+        
+        int n = num.length;
+
+        for(int i=0;i<num.length;i++){
+            System.out.print(num[i]+" ");
+        }
+
+        
+    }//main
+}//class
