2e4893cc022963140aa2a66159c2731a7d03c035
SHREYANK <66244570+bshreyank@users.noreply.github.com>
2023-04-02 22:11:00 +0530

    commit

--- Methods/c_Claci.java
+++ Methods/c_Claci.java
@@ -2,9 +2,10 @@
  * Create a method named calculator that will accept an array of 5 numbers and return 
  * the addition, subtraction, multiplication of the elements in another array.  
  */
-
-
 package Methods;
+import java.util.Scanner; 
+
+
 
 public class c_Claci {
 
@@ -43,7 +44,64 @@ public class c_Claci {
   }//main
   
   *///AIAIAIAIAIAIAIAIAIAIAIAIAIAIAIAIAIAIAIAIAIAIAIAIAIAIAIAIAIAIAI
+  
+  
+   void calculate() 
+  { 
+    
+    Scanner sc=new Scanner(System.in); 
+    System.out.println("Enter The number in the array "); 
+    int v=sc.nextInt(); 
 
+    int[] arr=new int[5]; 
+
+    int sum=0; 
+    int sum1=1; 
+    int i; 
+    int diff=arr[0]; 
+
+
+    System.out.println("Enter the element of the array "); 
+    for( i=0;i<arr.length;i++) { 
+     arr[i]=sc.nextInt(); 
+    } 
+
+
+    System.out.println("-----------------------------------------------------------------------"); 
+
+
+    System.out.println("the Addition of the array Element"); 
+    for( i=0;i<arr.length;i++) { 
+     sum=sum+arr[i]; 
+    } 
+    System.out.println("the Addition of the array is : "+sum); 
+
+
+    System.out.println("-----------------------------------------------------------------------"); 
+     
+    System.out.println(" the multipication of the array Element "); 
+    for( i=0;i<arr.length;i++) { 
+     sum1=sum1*arr[i]; 
+    } 
+    System.out.println(" the multipication of the array is: "+sum1); 
+
+
+    System.out.println("-----------------------------------------------------------------------"); 
+
+
+    System.out.println("the Subtraction of the array Element"); 
+    for(i=0;i<arr.length;i++) { 
+    if(i>0) { 
+     diff-=arr[i]; 
+    }} 
+    System.out.println("the Subtraction of the array is: "+diff); 
+    
+  } 
+   public static void main(String arr[]) { 
+    c_Claci obj=new c_Claci(); 
+   obj.calculate(); 
+   } 
+   
 
 
 }//Class claci
