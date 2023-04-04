cc379807b94b4648371e334f18e1585d11f05178
SHREYANK <66244570+bshreyank@users.noreply.github.com>
2023-04-04 19:50:12 +0530

    new sol

--- Methods/d_CarAttribute.java
+++ Methods/d_CarAttribute.java
@@ -1,5 +1,37 @@
 package Methods;
+import java.util.Scanner;
+/*
+ * Create a class that has attributes about a car
+ * including average, create methods that will calculate 
+ * and return distance that can be covered by 
+ * the car accepting the amount of fuel it has
+ */
+class car{
+	int average =20 ;
+	 void distance() 
+     {
+		 int fuel;
+		 int dist;
+
+		 Scanner sc = new Scanner(System.in);
+		 System.out.println("Enter the value of fuel inserted in Litre :"  );
+		 fuel = sc.nextInt();
+
+		 dist = fuel*average;
+		
+         System.out.println("Distance travelled by car is " +dist+"km");
+	}
+}
 
 public class d_CarAttribute {
-    
+    public static void main(String[] args) {
+		car obj = new car();
+		obj.distance();
+    }
 }
+
+
+	
+
+	
+
