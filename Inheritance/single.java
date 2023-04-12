1908974eb8feeee46028ea4791a39e15db455bf4
SHREYANK <66244570+bshreyank@users.noreply.github.com>
2023-04-12 20:05:41 +0530

    new

---
+++ Inheritance/single.java
@@ -0,0 +1,21 @@
+package Inheritance;
+
+class person{
+    String name;    
+    int age;
+    void printer(){        
+        System.out.println(name);
+    }
+}
+class student extends person{    
+    int rollno;
+}
+
+public class inheritance {
+    public static void main(String args[]){        
+        student shreyank = new student();
+        shreyank.rollno = 1234;        
+        shreyank.name = "Shreyank";
+        System.out.println(shreyank.name);        
+        shreyank.printer(); //Methods can be called in Inheritance
+    }}
--- Methods/d_CarAttribute.java
+++ Methods/d_CarAttribute.java
@@ -1,35 +1,33 @@
 package Methods;
-import java.util.Scanner;
+
 /*
  * Create a class that has attributes about a car
  * including average, create methods that will calculate 
  * and return distance that can be covered by 
  * the car accepting the amount of fuel it has
  */
-class car{
-	int average =20 ;
-	 void distance() 
-     {
-		 int fuel;
-		 int dist;
-
-		 Scanner sc = new Scanner(System.in);
-		 System.out.println("Enter the value of fuel inserted in Litre :"  );
-		 fuel = sc.nextInt();
-
-		 dist = fuel*average;
-		
-         System.out.println("Distance travelled by car is " +dist+"km");
+class car {
+	int average;
+	void distance_possible(int fuel) {
+		System.out.println("possible distance is - " + (fuel * average));
 	}
+	int rem_fuel(int dist, int ini_fuel) {
+		int consumed_fuel = dist/average;
+		return (ini_fuel - consumed_fuel);
+	}
+	
 }
 
 public class d_CarAttribute {
-    public static void main(String[] args) {
-		car obj = new car();
-		obj.distance();
-    }
+	public static void main(String[] args) {
+		car city = new car();
+		city.average = 30;
+		city.distance_possible(12);
+		int a = city.rem_fuel(50, 5);
+		System.out.println("remainin fuel is - " + a);
+		
+	}
 }
-
 
 	
 
