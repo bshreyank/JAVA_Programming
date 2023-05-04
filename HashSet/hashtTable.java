35eebbc0df42eb18b100f34c28a6d3864c9d23d1
SHREYANK <66244570+bshreyank@users.noreply.github.com>
2023-05-04 19:00:25 +0530

    new

---
+++ HashSet/hashtTable.java
@@ -0,0 +1,18 @@
+package HashSet;
+
+import java.util.HashMap;
+
+public class hashtTable {
+
+  public static void main(String[] args) {
+    HashMap<String, String> names = new HashMap<>();
+    names.put("Shreyas", "Patil");
+    names.put("Ruchita", "Bisen");
+    names.put("Priyanka", "Pawar");
+        System.out.println(names);
+    //    System.out.println(names.get("Shreyas"));//    System.out.println(names.size());
+    /*for (String i : names.values()) {
+      System.out.println(i);
+    }*/
+  }
+}
